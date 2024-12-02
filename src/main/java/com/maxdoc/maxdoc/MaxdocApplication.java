08840ce7e1a63b8bddb72130f6a777dd81ec72e1
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-02 13:34:06 -0300

    commit inicial: cria projeto spring

---
+++ src/main/java/com/maxdoc/maxdoc/MaxdocApplication.java
@@ -0,0 +1,13 @@
+package com.maxdoc.maxdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxdocApplication.class, args);
+	}
+
+}
