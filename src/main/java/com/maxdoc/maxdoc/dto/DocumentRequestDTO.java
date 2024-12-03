0c3f31116e7729ac89a7457d12829f6b1aed147d
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-03 17:25:53 -0300

    feat: substitui getters e setters pelas assinaturas do lambok

--- src/main/java/com/maxdoc/maxdoc/dto/DocumentRequestDTO.java
+++ src/main/java/com/maxdoc/maxdoc/dto/DocumentRequestDTO.java
@@ -1,43 +1,16 @@
 package com.maxdoc.maxdoc.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class DocumentRequestDTO {
     private String title;
     private String description;
     private String sigla;
     private int version;
 
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSigla() {
-        return sigla;
-    }
-
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public void setVersion(int version) {
-        this.version = version;
-    }
 
 }
 
--- src/main/java/com/maxdoc/maxdoc/dto/DocumentResponseDTO.java
+++ src/main/java/com/maxdoc/maxdoc/dto/DocumentResponseDTO.java
@@ -1,8 +1,13 @@
 package com.maxdoc.maxdoc.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Setter
+@Getter
 public class DocumentResponseDTO {
     private UUID id;
     private String title;
@@ -11,42 +16,5 @@ public class DocumentResponseDTO {
     private int version;
     private String phase;
 
-
-    public UUID getId() {
-        return id;
-    }
-    public void setId(UUID id) {
-        this.id = id;
-    }
-    public String getPhase() {
-        return phase;
-    }
-    public void setPhase(String phase) {
-        this.phase = phase;
-    }
-    public String getSigla() {
-        return sigla;
-    }
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
-    public int getVersion() {
-        return version;
-    }
-    public void setVersion(int version) {
-        this.version = version;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
 }
