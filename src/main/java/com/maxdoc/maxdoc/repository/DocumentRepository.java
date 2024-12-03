89c8577eab67940b81a1bb8a60887c9e49de90ee
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-03 14:22:54 -0300

    feat: altera o tipo do id LONG -> UUID na interface DocumentRepository

--- src/main/java/com/maxdoc/maxdoc/repository/DocumentRepository.java
+++ src/main/java/com/maxdoc/maxdoc/repository/DocumentRepository.java
@@ -4,7 +4,8 @@ import com.maxdoc.maxdoc.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface DocumentRepository extends JpaRepository<Document, Long> {
+public interface DocumentRepository extends JpaRepository<Document, UUID> {
     Optional<Document> findBySiglaAndVersion(String sigla, int version);
 }
