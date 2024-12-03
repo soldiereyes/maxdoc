4a2d7523eff95ead845d92bdc205f283889abcd3
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-03 14:23:19 -0300

    feat: cria a DocumentRequestDTO

---
+++ src/main/java/com/maxdoc/maxdoc/dto/DocumentRequestDTO.java
@@ -0,0 +1,8 @@
+package com.maxdoc.maxdoc.dto;
+
+public class DocumentRequestDTO {
+    private String title;
+    private String description;
+    private String sigla;
+    private int version;
+}
