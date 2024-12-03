9b2ca2a47714ee8284c63fdf053a88800b6269ae
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-03 17:19:16 -0300

    feat: cria getters e setter para o DTO de Response

--- src/main/java/com/maxdoc/maxdoc/dto/DocumentResponseDTO.java
+++ src/main/java/com/maxdoc/maxdoc/dto/DocumentResponseDTO.java
@@ -10,4 +10,43 @@ public class DocumentResponseDTO {
     private String sigla;
     private int version;
     private String phase;
+
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getPhase() {
+        return phase;
+    }
+    public void setPhase(String phase) {
+        this.phase = phase;
+    }
+    public String getSigla() {
+        return sigla;
+    }
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+    public int getVersion() {
+        return version;
+    }
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
