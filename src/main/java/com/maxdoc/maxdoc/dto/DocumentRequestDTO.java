1538d0335af1d8555d0f5ef115b3371b524a82b1
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-03 17:19:06 -0300

    feat: cria getters e setter para o DTO de Request

--- src/main/java/com/maxdoc/maxdoc/dto/DocumentRequestDTO.java
+++ src/main/java/com/maxdoc/maxdoc/dto/DocumentRequestDTO.java
@@ -5,4 +5,40 @@ public class DocumentRequestDTO {
     private String description;
     private String sigla;
     private int version;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
 }
+
+
