05ca298d0abd27134e4e7e17f32d6f91f42e16b9
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-06 12:12:38 -0300

    feat: implementa a assinatura para conexão com o front

--- src/main/java/com/maxdoc/maxdoc/controller/DocumentController.java
+++ src/main/java/com/maxdoc/maxdoc/controller/DocumentController.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/documents")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DocumentController {
 
     private final DocumentService documentService;
